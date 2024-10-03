7ff1ed15011731e2951b30dae3f52e475e31e75d
Fermn <46830086+Fermn@users.noreply.github.com>
2024-10-03 07:03:49 +0000

    add validation for letters and spaces on title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ApplicationRecord
-    validates :title, presence: true, uniqueness: true
+    validates :title, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
 end
