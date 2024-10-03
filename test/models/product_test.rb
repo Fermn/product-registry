de82a27af430d2ef275435b27723898307a0880f
Fermn <46830086+Fermn@users.noreply.github.com>
2024-10-03 07:05:24 +0000

    create test cases for product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product = Product.new(title: "Sample Product")
+  end
+
+  test "should be valid with valid attributes" do
+    assert @product.valid?
+  end
+
+  test "should not be valid without a title" do
+    @product.title = nil
+    assert_not @product.valid?
+  end
+
+  test "should not be valid with a duplicate title" do
+    duplicate_product = @product.dup
+    @product.save!
+    assert_not duplicate_product.valid?
+  end
+
+  test "should not be valid with special characters in title" do
+    @product.title = "Invalid@Title!"
+    assert_not @product.valid?
+  end
+
+  test "should not be valid with numbers in title" do
+    @product.title = "Title123"
+    assert_not @product.valid?
+  end
 end
