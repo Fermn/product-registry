4c18bb663a2b00eb10a40d26ed7d948d3862b631
Fermin <46830086+Fermn@users.noreply.github.com>
2024-10-04 00:11:09 +0000

    create callback to format title before save

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
     validates :title, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
+
+    before_save :format_title
+
+    private
+
+    def format_title
+      self.title = title.strip.downcase
+    end
 end
